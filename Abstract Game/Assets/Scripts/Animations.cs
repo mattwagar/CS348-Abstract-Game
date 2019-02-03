3ac2d064052d592f97d83466c4928c80781f9bc7
Andrew Kennedy <albinospringroll@aestusstudios.com>
2019-02-03 12:34:43 -0500

    Created a Spawner for Enemies, and the first enemy prefab

---
+++ Abstract Game/Assets/Scripts/Animations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    public int rotateX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotator();
+        GrowShrink();
+
+    }
+
+    private void GrowShrink()
+    {
+        
+    }
+
+    private void Rotator()
+    {
+        transform.Rotate(Vector3.right * rotateX * Time.deltaTime);
+    }
+}
