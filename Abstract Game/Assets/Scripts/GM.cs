61cec9423b83fcbcb2ff45c1d4418c42e2eea54b
Joshua Karmel <callmeshua620@gmail.com>
2019-02-05 00:41:11 -0500

    Start Screen and game manager

---
+++ Abstract Game/Assets/Scripts/GM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+    public GameObject game, startScreen;
+    public Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(begin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            startScreen.SetActive(false);
+            game.SetActive(true);
+        }
+    }
+
+    void begin(){
+            startScreen.SetActive(false);
+            game.SetActive(true);
+    }
+}
