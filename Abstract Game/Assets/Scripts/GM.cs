098cc340480237d6896c7cb593c885f916720867
callmeshua <callmeshua620@gmail.com>
2019-02-05 16:22:59 -0500

    timer added

--- Abstract Game/Assets/Scripts/GM.cs
+++ Abstract Game/Assets/Scripts/GM.cs
@@ -5,26 +5,45 @@ using UnityEngine.UI;
 
 public class GM : MonoBehaviour
 {
-    public GameObject game, startScreen;
+    public GameObject game, startScreen, inGameCanvas;
     public Button startButton;
+    public Text timerText;
+    private float timer;
+    private bool started;
 
     // Start is called before the first frame update
     void Start()
     {
         startButton.onClick.AddListener(begin);
+        startScreen.SetActive(true);
+        game.SetActive(false);
+        inGameCanvas.SetActive(false);
+        started = false;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
+            started = true;
             startScreen.SetActive(false);
             game.SetActive(true);
+            inGameCanvas.SetActive(true);
         }
+
+        if(started)
+        {
+            timer += Time.deltaTime;
+            timerText.text = timer.ToString("F2");
+        }
+
     }
 
     void begin(){
+            started = true;
             startScreen.SetActive(false);
             game.SetActive(true);
+            inGameCanvas.SetActive(true);
     }
 }
